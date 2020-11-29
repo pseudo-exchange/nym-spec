8f5e6cc96718cc8832c5b1f11c0c211084e5bae8
Trevor Clarke <trevorjtclarke@gmail.com>
2020-11-28 16:29:53 -0800

    Initial setup

---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use near_sdk::{env, near_bindgen};
+use near_sdk::collections::UnorderedMap;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct Thing {
+    records: UnorderedMap<String, String>,
+}
+
+#[near_bindgen]
+impl Thing {
+    pub fn set_thing(&mut self, msg: String) {
+        env::log(b"A");
+        let account_id = env::signer_account_id();
+        self.records.insert(&account_id, &msg);
+    }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use near_sdk::MockedBlockchain;
+    use near_sdk::{testing_env, VMContext};
+
+    fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
+        VMContext {
+            current_account_id: "alice_near".to_string(),
+            signer_account_id: "bob_near".to_string(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id: "carol_near".to_string(),
+            input,
+            block_index: 0,
+            block_timestamp: 0,
+            account_balance: 0,
+            account_locked_balance: 0,
+            storage_usage: 0,
+            attached_deposit: 0,
+            prepaid_gas: 10u64.pow(18),
+            random_seed: vec![0, 1, 2],
+            is_view,
+            output_data_receivers: vec![],
+            epoch_height: 0,
+        }
+    }
+
+    #[test]
+    fn get_nonexistent_message() {
+        let context = get_context(vec![], true);
+        testing_env!(context);
+        let contract = StatusMessage::default();
+        assert_eq!(None, contract.get_status("francis.near".to_string()));
+    }
+}
